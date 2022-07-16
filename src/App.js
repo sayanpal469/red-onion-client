2ce91f72a010dfa893e91504f7308e572be4fde6
sayanpal469 <sayanpal469@gmail.com>
2022-07-16 16:09:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Shared/Nav/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
